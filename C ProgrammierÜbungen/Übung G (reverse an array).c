a024633cda1efbc60631776e11eb1b4c463feeab
David 14 <deinemeit@gmail.com>
2022-02-18 12:30:50 +0100

    Übung G Fertig

--- C ProgrammierÜbungen/Übung G (reverse an array).c
+++ C ProgrammierÜbungen/Übung G (reverse an array).c
@@ -10,5 +10,10 @@ int main()
 
 void reverse(int arr[], int n)
 {
-
+    for (int low = 0, high = n - 1; low < high; low++, high--)
+    {
+        int temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;
+    }
 }
