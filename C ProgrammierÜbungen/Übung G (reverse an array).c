22dd819fd9855ff5aea80ff1edcb7fe6ea2ca978
David 14 <deinemeit@gmail.com>
2022-02-19 11:46:03 +0100

    Main auskommentiert

--- C ProgrammierÜbungen/Übung G (reverse an array).c
+++ C ProgrammierÜbungen/Übung G (reverse an array).c
@@ -2,11 +2,11 @@
 
 void reverse(int arr[], int n);
 
-int main()
-{
-	int arr[] = {1,2,3,4,5,6,7,9};
-	reverse(arr, 8);
-}
+//int main()
+//{
+//	int arr[] = {1,2,3,4,5,6,7,9};
+//	reverse(arr, 8);
+//}
 
 void reverse(int arr[], int n)
 {
