05f2f267637918b1e4f698344c4e01b153dda167
David 14 <deinemeit@gmail.com>
2022-02-21 08:13:01 +0100

    Übung 4 hinzugefügt

---
+++ C ProgrammierÜbungen/Übung 4 C (Fakultät).c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//int main()
+//{
+//	int fak = 0;
+//	int startvalue = 0;
+//	printf("Gib mir die Fakultaet: ");
+//	scanf_s("%d", &fak);
+//	
+//	if (fak < 1)
+//	{
+//		printf("1");
+//		return 0;
+//	}
+//	startvalue = fak;
+//	for (int i = 1; i < startvalue; i++)
+//	{
+//		fak *= i;
+//
+//	}
+//	printf("%d", fak);
+//}
+
+
---
+++ C ProgrammierÜbungen/Übung 4 D (Leipniz Reihe und mehr).c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+
+//int main() {
+	//	
+	//	/*double sum = 1;
+	//	for (double i = 2; i <= 100000; i++)
+	//	{
+	//		double num = (1 / i);
+	//		sum += num;
+
+	//	}
+	//	printf("%.16lf",sum);*/
+
+	//	double pi = 1;
+	//	double plusnumber = 3;
+	//	for (double i = 1; i <= 1000000000; i++)
+	//	{
+	//		double num = (1 / plusnumber);
+	//		pi -= num;
+	//		plusnumber += 2;
+	//		num = (1 / plusnumber);
+	//		pi += num;
+	//		plusnumber += 2;
+
+
+	//	}
+
+	//	printf("%.11lf\n", pi); 
+
+	//	//Das nicht gut glaub ich
+	//	/*pi = 1;
+	//	for (double i = 3; i <= 1000000; i+=2)
+	//	{
+	//		double num = (1 / i);
+	//		pi -= num;
+	//		i += 2;
+	//		num = (1 / i);
+	//		pi += num;
+
+
+	//	}
+
+	//	printf("%.11lf", pi);*/
+
+	//}
+
+
